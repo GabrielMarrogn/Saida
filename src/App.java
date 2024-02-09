bfeab7342854b6f401d979460659b36fd9122ffc
GabrielMarrogn <gabrielosantos2012@hotmail.com>
2024-02-09 17:54:39 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        for(int i = 1; i <= 7; i++){
+            if(i == 1 || i == 7){
+                for(int k = 0;k < 39; k++){
+                    System.out.print("-");
+                }
+            }else if(i > 1 && i <7){
+                for(int k = 0;k < 39; k++){
+                    if(k == 0 || k == 38){
+                        System.out.print("|");
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
